e46792606c3f6c8b344b15383a510eaed76c7e80
Guillaume Havé <guillaume.have@orange.fr>
2024-10-30 20:52:25 +0100

    Add testing for textnode class

--- src/main.py
+++ src/main.py
@@ -2,7 +2,9 @@ from textnode import TextNode, TextType
 
 def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    node2 = TextNode("This is a text node", TextType.ITALIC)
     print(node)
+    print(node2)
 
 
 main()

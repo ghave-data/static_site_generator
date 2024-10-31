78f8254710363bd904c8c09dc2420dd98f9344de
Guillaume Havé <guillaume.have@orange.fr>
2024-10-31 17:39:11 +0100

    Add text to textnode

--- src/textnode.py
+++ src/textnode.py
@@ -117,4 +117,14 @@ def split_nodes_link(old_nodes):
             original_text = sections[1]
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
-    return new_nodes   
+    return new_nodes 
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+    
